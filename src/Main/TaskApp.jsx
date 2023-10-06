56f7d69d4e9c0110dac0ee7d84fbe81e6eef13a0
Mateus Henrique <mateusteccruz@gmail.com>
2023-10-05 22:06:36 -0300

    ok

--- src/Main/TaskApp.jsx
+++ src/Main/TaskApp.jsx
@@ -4,20 +4,36 @@ import style from '../Main/Task.module.css'
 import { FaTrash} from 'react-icons/fa'
 
 
+
 function TaskApp() {
-    const [currentPage, setCurrentPage] = useState("ativos");
+  const [currentPage, setCurrentPage] = useState("ativos");
+  const [item, setItem] = useState(''); //iniciado com string vazia, setItem serve p editar
+  const [itemList, setItemList] = useState([]); //iniciado com array vazio serve p setItemList editar lista
 
-    const [item, setItem] = useState(''); //iniciado com string vazia, setItem serve p editar
-    const [itemList, setItemList] = useState([]) //iniciado com array vazio serve p setItemList editar lista
-
-
-    
-    const addItem = () => {
-      if (item.trim() !== "") { // Verifica se o valor de item não está vazio
-        setItemList([item].concat(itemList))
-        setItem('')
+  const toggleTaskStatus = (itemToToggle) => {
+    const updatedItemList = itemList.map((item) => {
+      if (item === itemToToggle) {
+        return { ...item, completed: !item.completed };
       }
-     
+      return item;
+    });
+    setItemList(updatedItemList);
+  };
+
+  const filteredTasks = itemList.filter((item) => {
+    if (currentPage === "ativos") {
+      return !item.completed;
+    } else if (currentPage === "concluídos") {
+      return item.completed;
+    }
+    return true;
+  });
+
+  const addItem = () => {
+    if (item.trim() !== "") { // Verifica se o valor de item não está vazio
+      setItemList([{ text: item, completed: false }, ...itemList]);
+      setItem('');
+    }
   }
 
   const deleteItem = (itemToDelete) => {
@@ -25,34 +41,47 @@ function TaskApp() {
     setItemList(updatedItemList);
   };
 
-
-
   return (
     <>
-     <div className={style.AppMain}>
-      <h1>Lista de Tarefas</h1>
-      <input type="text" placeholder="tarefa" value={item} name="item" onChange = {e => setItem(e.target.value)}/><br/>
-      <button onClick={addItem}>Adicionar tarefa</button>
-      <ul>
-      {itemList.map((item) => (
-      <li key={item} className={style.listItem}>
-          {item}{' '}
-          <div className={style.itemActions}>
-          <input type="checkbox" name="completed"  className={style.buttonCheckBox} />
-          <button className={style.buttonTrash} onClick={() => deleteItem(item)}>
-                <FaTrash className={style.TrashIcon} />
-            </button>
-            </div>
-        </li>
-
-       /*   <li>{item}  <FaTrash/></li>*/
-        ))}
-      </ul>
-        
-
+      <div className={style.AppMain}>
+        <h1>Lista de Tarefas</h1>
+        <input
+          type="text"
+          placeholder="tarefa"
+          value={item}
+          name="item"
+          onChange={(e) => setItem(e.target.value)}
+        /><br />
+        <button onClick={addItem}>Adicionar tarefa</button>
+        <ul>
+          {filteredTasks.map((item) => (
+            <li key={item.text} className={style.listItem}>
+              {item.text}{' '}
+              <div className={style.itemActions}>
+                <input
+                  type="checkbox"
+                  name="completed"
+                  className={style.buttonCheckBox}
+                  checked={item.completed}
+                  onChange={() => toggleTaskStatus(item)}
+                />
+                <button
+                  className={style.buttonTrash}
+                  onClick={() => deleteItem(item)}
+                >
+                  <FaTrash className={style.TrashIcon} />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <button onClick={() => setCurrentPage("ativos")}>Tarefas Ativas</button>
+          <button onClick={() => setCurrentPage("concluídos")}>Tarefas Concluídas</button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default TaskApp;
--- src/Main/TaskList.jsx
+++ src/Main/TaskList.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+// TaskList.js
 
-function TaskList({ tasks }) {
+import React from 'react';
+import { FaTrash } from 'react-icons/fa';
+
+function TaskList({ tasks, toggleTaskStatus, deleteTask }) {
   return (
     <ul>
-      {tasks.map((item) => (
-        <li key={item}>
-          {item}
+      {tasks.map((task, index) => (
+        <li key={index}>
+          <input
+            type="checkbox"
+            checked={task.completed}
+            onChange={() => toggleTaskStatus(index)}
+          />
+          <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+            {task.text}
+          </span>
+          <button onClick={() => deleteTask(index)}>
+            <FaTrash />
+          </button>
         </li>
       ))}
     </ul>
